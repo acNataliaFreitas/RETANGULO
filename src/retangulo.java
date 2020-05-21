87d5f0b8e12aca3bc57f12975b38243fbae5a393
Natália Freitas <natyff1@hotmail.com>
2020-05-20 22:31:52 -0300

    Programa finalizado

--- src/retangulo.java
+++ src/retangulo.java
@@ -9,6 +9,8 @@ public class retangulo {
 		Scanner sc = new Scanner(System.in);
 		
 		double largura,altura,ContaArea,ContaPerimetro, opcao;
+		int fim;
+		fim=0;
 	
 		//LARGURA
 		System.out.println("Digite a largura do retangulo:");
@@ -24,10 +26,46 @@ public class retangulo {
 		while(altura< 0.0) {
 			System.out.println("O valor deve ser positivo. Tente novamente:");
 			altura=sc.nextDouble();
+		}	
+		
+		//MENU
+
+		while(fim == 0) {
+		System.out.println("MENU");
+		System.out.println("1 - Mostrar area");
+		System.out.println("2 - Mostrar perimetro");
+		System.out.println("3 - Sair");
+		
+		System.out.println("Digite uma opçao :");
+		opcao=sc.nextDouble();
+		
+			if(opcao==1) {
+				ContaArea= largura *altura;
+				System.out.printf("AREA = %.2f%n",ContaArea);
+				System.out.println("Digite uma opçao :");
+				opcao=sc.nextDouble();
+			}
+			
+			else if(opcao==2) {
+				ContaPerimetro = 2*largura + 2*altura;
+				System.out.printf("PERIMETRO = %.2f%n", ContaPerimetro);
+				System.out.println("Digite uma opçao :");
+				opcao=sc.nextDouble();
+			}
+			
+			else if(opcao==3){
+				System.out.println("OPÇAO INVALIDA");
+				fim=1;
+			}
+			
+			else {
+				System.out.println("FIM DO PROGRAMA!");
+			}
+		
 		}
-		
-		
+			
 		sc.close();
+		
+}	
 	}
 
-}
